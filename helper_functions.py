fda4f8d41c476c74d07a609f97e892e2cd8134c7
Suvadip2776 <ss2776@cornell.edu>
2025-04-01 01:38:17 -0400

    added a helper function

--- helper_functions.py
+++ helper_functions.py
@@ -1,3 +1,8 @@
 #This files contains some helper functions
+def flatten_ballot_ranking(ballot):
+    """
+    Convert a VoteKit ballot's ranking (stored as a tuple of frozensets)
+    into a simple list (assumes each frozenset contains exactly one candidate).
+    """
+    return [next(iter(fs)) for fs in ballot.ranking]
 
-import os
