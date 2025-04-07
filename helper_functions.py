a7d89dd7e00a18cb0353f56fb865d67ab22ffb01
Suvadip2776 <ss2776@cornell.edu>
2025-04-07 13:41:20 -0400

    uploaded IIA code for scottish data

--- helper_functions.py
+++
@@ -1,8 +0,0 @@
-#This files contains some helper functions
-def flatten_ballot_ranking(ballot):
-    """
-    Convert a VoteKit ballot's ranking (stored as a tuple of frozensets)
-    into a simple list (assumes each frozenset contains exactly one candidate).
-    """
-    return [next(iter(fs)) for fs in ballot.ranking]
-
