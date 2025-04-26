3e51d388376427868fe3f7ebf3142ca8b3542590
Suvadip2776 <ss2776@cornell.edu>
2025-04-26 18:35:33 -0400

    Pluraliy ranking voting rules added

--- Voting_rules.py
+++ Voting_rules.py
@@ -27,3 +27,28 @@ def Ranked_Borda(profile):
         ranking_list.extend(list(group))  # groups can have ties (more than one candidate)
     
     return ranking_list
+
+def Ranked_Plurality(profile):
+    """
+    Given a PreferenceProfile, returns the complete Borda ranking as a list.
+    
+    Args:
+        profile (PreferenceProfile): The preference profile with ballots and candidates.
+    
+    Returns:
+        list: Candidates ranked from winner to last place according to Borda scores.
+    """
+
+    election = Plurality(profile, m= len(profile.candidates))
+    
+    # Get the full ranking (tuple of frozensets)
+    plurality_ranking = election.get_ranking(-1)
+    
+    # Flatten the frozensets into a list
+    ranking_list = []
+    for group in plurality_ranking:
+        ranking_list.extend(list(group))  # groups can have ties (more than one candidate)
+    
+    return ranking_list
+
+
