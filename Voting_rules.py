d6d3e2ef6129a64d587ff6fdb1493152518745ad
Suvadip2776 <ss2776@cornell.edu>
2025-05-06 08:29:13 -0400

    Added two voting rules

--- Voting_rules.py
+++ Voting_rules.py
@@ -1,4 +1,4 @@
-#This is the voting_rules script made for havng the modified voting rules
+#This is the voting_rules script made for the modified voting rules (outputting the complete ranking)
 
 #Make sure votekit is installed
 
@@ -50,5 +50,63 @@ def Ranked_Plurality(profile):
         ranking_list.extend(list(group))  # groups can have ties (more than one candidate)
     
     return ranking_list
+
+def Ranked_2_Approval(profile):
+    """
+    Given a PreferenceProfile, returns the complete Borda ranking as a list.
+    
+    Args:
+        profile (PreferenceProfile): The preference profile with ballots and candidates.
+    
+    Returns:
+        list: Candidates ranked from winner to last place according to Borda scores.
+    """
+   
+    M = len(profile.candidates)
+    
+    # Construct the 2-approval score vector: 1 for top 2, 0 for others
+    approval_vector = [1 if i < 2 else 0 for i in range(M)]
+
+    election = Borda(profile, m= len(profile.candidates), score_vector = approval_vector)
+    
+    # Get the full ranking (tuple of frozensets)
+    borda_ranking = election.get_ranking(-1)
+    
+    # Flatten the frozensets into a list
+    ranking_list = []
+    for group in borda_ranking:
+        ranking_list.extend(list(group))  # groups can have ties (more than one candidate)
+    
+    return ranking_list
+
+def Ranked_3_Approval(profile):
+    """
+    Given a PreferenceProfile, returns the complete Borda ranking as a list.
+    
+    Args:
+        profile (PreferenceProfile): The preference profile with ballots and candidates.
+    
+    Returns:
+        list: Candidates ranked from winner to last place according to Borda scores.
+    """
+   
+    M = len(profile.candidates)
+    
+    # Construct the 3-approval score vector: 1 for top 3, 0 for others
+    approval_vector = [1 if i < 3 else 0 for i in range(M)]
+
+    election = Borda(profile, m= len(profile.candidates), score_vector = approval_vector)
+    
+    # Get the full ranking (tuple of frozensets)
+    borda_ranking = election.get_ranking(-1)
+    
+    # Flatten the frozensets into a list
+    ranking_list = []
+    for group in borda_ranking:
+        ranking_list.extend(list(group))  # groups can have ties (more than one candidate)
+    
+    return ranking_list
+
+
 
 
