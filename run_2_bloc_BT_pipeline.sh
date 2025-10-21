a067958d230fc252e144a83c4f0df72c4c83c9d5
peterrrock2 <27579114+peterrrock2@users.noreply.github.com>
2025-10-20 22:08:07 -0600

    Update pipeline script to accurately refelct what was run

--- run_2_bloc_BT_pipeline.sh
+++ run_2_bloc_BT_pipeline.sh
@@ -9,71 +9,69 @@ if [[ $? -ne 0 ]]; then
     exit 1
 fi
 
-n_seats=3
-for metric in "sigma_UM" "sigma_UM_winner_set"
-do
-    for variant in "worst_case" "average"
-    do
-        for interpolation_type in "asin" "odds" "linear"
-        do
-            for election_type in "borda" "plurality" "stv"
-            do
-                uv run pipelines/bradley-terry/collect_stats_BT.py \
-                    --input-folder $input_folder \
-                    --n-seats $n_seats \
-                    --metric $metric \
-                    --variant $variant \
-                    --interpolation-type $interpolation_type \
-                    --election-type $election_type
-            done
-        done
-    done
-done
+# input_folder="${SCRIPT_DIR}/./data/preference_profiles/05_05/b_proportion_0.7__ALPHA_(1.00,1.00,1.00,1.00)__COHESION_(0.70,0.70)/"
 
-for metric in "sigma_IIA" "sigma_IIA_winner_set"
-do
-    for variant in "worst_case" "average"
-    do
-        for interpolation_type in "None"
-        do
-            for election_type in "borda" "plurality" "stv"
-            do
-                uv run pipelines/bradley-terry/collect_stats_BT.py \
-                    --input-folder $input_folder \
-                    --n-seats $n_seats \
-                    --metric $metric \
-                    --variant $variant \
-                    --interpolation-type $interpolation_type \
-                    --election-type $election_type
-            done
-        done
-    done
-done
+# for input_folder in $(find ${SCRIPT_DIR}/data/preference_profiles/ -type d -name "*b_proportion*"); do
+#     echo "Processing input folder: $input_folder"
 
-# NOTE: Uncomment me to run the all subset metrics. These will take a LONG time to run,
-# so they might be better relegated to a cluster computer
-
-# for metric in "sigma_IIA_all_subset" "sigma_IIA_winner_set_all_subset"
-# do
-#     for variant in "worst_case" "average"
-#     do
-#         for interpolation_type in "None"
-#         do
-#             for election_type in "borda" "plurality" "stv"
-#             do
-#                 uv run pipelines/bradley-terry/collect_stats_BT.py \
-#                     --input-folder $input_folder \
-#                     --n-seats $n_seats \
-#                     --metric $metric \
-#                     --variant $variant \
-#                     --interpolation-type $interpolation_type \
-#                     --election-type $election_type
+#     n_seats=3
+#     for metric in "sigma_UM" "sigma_UM_winner_set"; do
+#         for variant in "worst_case" "average"; do
+#             for interpolation_type in "asin" "odds" "linear"; do
+#                 for election_type in "borda" "plurality" "stv"; do
+#                     uv run pipelines/bradley-terry/collect_stats_BT.py \
+#                         --input-folder $input_folder \
+#                         --n-seats $n_seats \
+#                         --metric $metric \
+#                         --variant $variant \
+#                         --interpolation-type $interpolation_type \
+#                         --election-type $election_type
+#                 done
 #             done
 #         done
 #     done
-# done
 
+#     for metric in "sigma_IIA" "sigma_IIA_winner_set"; do
+#         for variant in "worst_case" "average"; do
+#             for interpolation_type in "None"; do
+#                 for election_type in "borda" "plurality" "stv"; do
+#                     uv run pipelines/bradley-terry/collect_stats_BT.py \
+#                         --input-folder $input_folder \
+#                         --n-seats $n_seats \
+#                         --metric $metric \
+#                         --variant $variant \
+#                         --interpolation-type $interpolation_type \
+#                         --election-type $election_type
+#                 done
+#             done
+#         done
+#     done
+
+#     # NOTE: Uncomment me to run the all subset metrics. These will take a LONG time to run,
+#     # so they might be better relegated to a cluster computer
+
+#     # for metric in "sigma_IIA_all_subset" "sigma_IIA_winner_set_all_subset"
+#     # do
+#     #     for variant in "worst_case" "average"
+#     #     do
+#     #         for interpolation_type in "None"
+#     #         do
+#     #             for election_type in "borda" "plurality" "stv"
+#     #             do
+#     #                 uv run pipelines/bradley-terry/collect_stats_BT.py \
+#     #                     --input-folder $input_folder \
+#     #                     --n-seats $n_seats \
+#     #                     --metric $metric \
+#     #                     --variant $variant \
+#     #                     --interpolation-type $interpolation_type \
+#     #                     --election-type $election_type
+#     #             done
+#     #         done
+#     #     done
+#     # done
+# done
 
 for f in $(find ${SCRIPT_DIR}/pipelines/bradley-terry/make_plots -name "*.py"); do
     uv run $f
 done
+
