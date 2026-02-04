bf91fbb714bc96fd9c94c52130c5b2ba84b7e1f7
peterrrock2 <27579114+peterrrock2@users.noreply.github.com>
2026-02-04 08:07:09 -0700

    Proportionality and proportionality plots

---
+++ pipelines/bradley-terry/compute_proportionality.py
@@ -0,0 +1,158 @@
+from votekit import RankProfile, PreferenceProfile
+from votekit.ballot_generator import BlocSlateConfig
+from glob import glob
+from joblib import Parallel, delayed
+from joblib_progress import joblib_progress
+import json
+from pathlib import Path
+import sys
+import click
+import pandas as pd
+from typing import get_args
+
+sys.path.append(str(Path(__file__).resolve().parents[2]))
+
+from voting_rules import build_voting_rule, AllowedRule
+
+TOP_DIR = Path(__file__).resolve().parents[2]
+
+
+def load_one(path) -> tuple[str, PreferenceProfile]:
+    return path, RankProfile.from_csv(path)
+
+
+def get_all_profiles(n_a_cand, n_b_cand, b_prop, a_coh, b_coh):
+    search_string = (
+        f"{TOP_DIR}/data/preference_profiles/{n_a_cand:02d}_{n_b_cand:02d}/"
+        f"b_proportion_{b_prop:0.1f}__ALPHA_(*)__COHESION_({a_coh:0.2f},{b_coh:0.2f})/*.csv"
+    )
+    profile_files = sorted(glob(search_string))
+
+    # profile_files = profile_files[:10]
+    with joblib_progress(total=len(profile_files)):
+        pairs = Parallel(n_jobs=-1, prefer="processes", batch_size="auto")(
+            delayed(load_one)(p) for p in profile_files
+        )
+
+    all_profiles = dict(pairs)
+    return all_profiles
+
+
+def frsp(r, s, rho):
+    return 1 - (1 - rho**s) / (1 - rho ** (r + s))
+
+
+def process_profile(file, profile) -> dict[str, dict[str, dict[str, float]]]:
+    utility_file = file.replace("preference_profiles", "preference_dfs").replace(
+        ".csv", "_utilities.csv"
+    )
+    utility_df = pd.read_csv(utility_file, index_col=0)
+    ranking_df = pd.read_csv(file, skiprows=list(range(8)))
+
+    a_cohesion, b_cohesion = list(
+        map(float, file.split("_COHESION_(")[1].split(")/")[0].split(","))
+    )
+    b_proportion = float(file.split("b_proportion_")[1].split("__")[0])
+
+    counts = {"A": 0, "B": 0}
+    slate_to_candidates = {"bloc_1": set(), "bloc_2": set()}
+
+    n_a_candidates, n_b_candidates = list(
+        map(int, file.split("preference_profiles/")[1].split("/")[0].split("_"))
+    )
+    n_cands = n_a_candidates + n_b_candidates
+    slate_to_candidates = {
+        "bloc_1": {f"A{i}" for i in range(1, n_a_candidates + 1)},
+        "bloc_2": {f"B{j}" for j in range(1, n_b_candidates + 1)},
+    }
+
+    for _, fpv, weight in ranking_df[["Ranking_1", "Weight"]].itertuples():
+        cand = fpv[2:4]
+        counts[cand[0]] += int(weight)
+
+    n_voters = int(ranking_df["Weight"].sum())
+
+    config = BlocSlateConfig(
+        n_voters=n_voters,
+        slate_to_candidates=slate_to_candidates,
+        cohesion_mapping={
+            "bloc_1": {"bloc_1": a_cohesion, "bloc_2": 1 - a_cohesion},
+            "bloc_2": {"bloc_1": 1 - b_cohesion, "bloc_2": b_cohesion},
+        },
+        bloc_proportions={"bloc_1": 1 - b_proportion, "bloc_2": b_proportion},
+        preference_mapping=utility_df,
+    )
+    config.normalize_preference_intervals()
+    n_b_voters = int(b_proportion * n_voters)
+    n_a_voters = n_voters - n_b_voters
+    n_a_candidates = len(slate_to_candidates["bloc_1"])
+    n_b_candidates = len(slate_to_candidates["bloc_2"])
+
+    prob_b_puts_b_first = frsp(
+        n_b_candidates, n_a_candidates, b_cohesion / (1 - b_cohesion)
+    )
+    prob_a_puts_a_first = frsp(
+        n_a_candidates, n_b_candidates, a_cohesion / (1 - a_cohesion)
+    )
+
+    expected_b_fpv = n_b_voters * prob_b_puts_b_first + n_a_voters * (
+        1 - prob_a_puts_a_first
+    )
+    expected_b_win_prorportion = expected_b_fpv / n_voters
+
+    file_to_rule_dict = {}
+    for voting_rule_name in get_args(AllowedRule):
+        rule = build_voting_rule(n_cands=n_cands, voting_rule_name=voting_rule_name)
+        winners = rule(profile, m=3).get_elected()
+        b_winners = [c for c in winners if next(iter(c)).startswith("B")]
+        b_win_proportion = len(b_winners) / len(winners)
+        file_to_rule_dict[voting_rule_name] = {
+            "expected_proportion": expected_b_win_prorportion,
+            "observed_proportion": b_win_proportion,
+        }
+
+    return {file: file_to_rule_dict}
+
+
+@click.command()
+@click.option("--n-a-cand", type=int, required=True, help="Number of A candidates")
+@click.option("--n-b-cand", type=int, required=True, help="Number of B candidates")
+@click.option(
+    "--b-prop",
+    type=float,
+    required=True,
+    help="Proportion of B voters (between 0 and 1)",
+)
+@click.option(
+    "--a-coh", type=float, required=True, help="Cohesion of A voters (between 0 and 1)"
+)
+@click.option(
+    "--b-coh", type=float, required=True, help="Cohesion of B voters (between 0 and 1)"
+)
+def main(n_a_cand, n_b_cand, b_prop, a_coh, b_coh):
+    all_profiles = get_all_profiles(n_a_cand, n_b_cand, b_prop, a_coh, b_coh)
+
+    with joblib_progress(total=len(all_profiles)):
+        all_file_to_rule_dicts = Parallel(
+            n_jobs=-1, prefer="processes", batch_size="auto"
+        )(
+            delayed(process_profile)(file, profile)
+            for file, profile in all_profiles.items()
+        )
+
+    total_dictionary: dict[str, dict[str, float]] = {}
+    for file_dict in all_file_to_rule_dicts:
+        total_dictionary.update(file_dict)
+
+    out_dir = Path(f"{TOP_DIR}/stats/bt_proportionality")
+    out_dir.mkdir(parents=True, exist_ok=True)
+    with open(
+        f"{out_dir}/{n_a_cand:02d}_{n_b_cand:02d}_bprop_"
+        f"{b_prop:0.1f}_cohesion_({a_coh:0.2f},{b_coh:0.2f})_results.json",
+        "w",
+    ) as f:
+        json.dump(total_dictionary, f, indent=4)
+
+
+if __name__ == "__main__":
+    main()
